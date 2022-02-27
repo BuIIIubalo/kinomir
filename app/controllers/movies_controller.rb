f393900ede4dc97bedf791c7b17eea5f7781c2bf
Vitaljs <003_66_003_20@mail.ru>
2022-02-27 16:27:53 +0600

    add: <Frontend&Backend> category page added

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,12 +4,35 @@ class MoviesController < ApplicationController
     @advise = Movie.where(recomended: true)
 
     #Categories
-    @pagy, @movies = pagy(Category.find_by_name('movie').movies)
-    @pagy, @series = pagy(Category.find_by_name('series').movies)
-    @pagy, @cartoons = pagy(Category.find_by_name('cartoon').movies)
+    @pagy, @movies = pagy(Category.find_by_name('movie').movies, items: 7)
+    @pagy, @series = pagy(Category.find_by_name('series').movies, items: 7)
+    @pagy, @cartoons = pagy(Category.find_by_name('cartoon').movies, items: 7)
   end
 
   def show
     @movie = Movie.find(params[:id]);
   end
+
+  # Categories
+
+  def getMovies
+    @pagy, @movies = pagy(Category.find_by_name('movie').movies, items: 35)
+    render :category
+  end
+
+  def getSeries
+    @pagy, @movies = pagy(Category.find_by_name('series').movies, items: 35)
+    render :category
+  end
+
+  def getCartoons
+    @pagy, @movies = pagy(Category.find_by_name('cartoon').movies, items: 35)
+    render :category
+  end
+
+  def getRecommendations
+    @pagy, @movies = pagy(Movie.where(:recomended => true), items: 35)
+    render :category
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   resources :movies
 
+  # Categories
+  get '/category-movies', :to => 'movies#getMovies'
+  get '/category-series', :to => 'movies#getSeries'
+  get '/category-cartoons', :to => 'movies#getCartoons'
+
+  get 'category-recommendations', :to => 'movies#getRecommendations'
+
   root "movies#index"
 end
